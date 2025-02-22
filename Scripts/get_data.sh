f891bb3f03ae2dfc62e68a521aaa531fb04b7736
Guillermo Comesaña Cimadevila <comesanaguillermo@gmail.com>
2025-02-22 19:24:06 +0000

    Add files via upload

---
+++ Scripts/get_data.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+# Download exposure GWAS data
+wget https://gwas.mrcieu.ac.uk/files/ieu-b-5067/ieu-b-5067.vcf.gz > exposure.vcf.gz
+
+# Download outcome GWAS data
+wget https://gwas.mrcieu.ac.uk/files/ieu-b-110/ieu-b-110.vcf.gz > outcome.vcf.gz
+
+echo "VCFs downloaded!"
+
+# gzunip VCFs
+gunzip exposure.vcf.gz
+gunzip outcome.vcf.gz
+
+echo "Process completed!"
