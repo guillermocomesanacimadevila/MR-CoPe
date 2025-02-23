d2c180bcc8710e56e8a7a31541cd41eff1fa0899
Guillermo Comesaña Cimadevila <comesanaguillermo@gmail.com>
2025-02-23 18:51:42 +0000

    Update get_data.sh

--- Scripts/get_data.sh
+++ Scripts/get_data.sh
@@ -1,15 +1,21 @@
 #!bin/bash
 
 # Download exposure GWAS data
-wget https://gwas.mrcieu.ac.uk/files/ieu-b-5067/ieu-b-5067.vcf.gz >> exposure.vcf.gz
+curl -O https://gwas.mrcieu.ac.uk/files/ieu-b-5067/ieu-b-5067.vcf.gz 
 
 # Download outcome GWAS data
-wget https://gwas.mrcieu.ac.uk/files/ieu-b-110/ieu-b-110.vcf.gz >> outcome.vcf.gz
+curl -O https://gwas.mrcieu.ac.uk/files/ieu-b-110/ieu-b-110.vcf.gz 
 
 echo "VCFs downloaded!"
 
 # gzunip VCFs
-gunzip exposure.vcf.gz
-gunzip outcome.vcf.gz
+gunzip ieu-b-110.vcf.gz
+gunzip ieu-b-5067.vcf.gz 
+
+echo "Unzipping done!"
+
+# Rename VCFs for clarity
+mv ieu-b-110.vcf exposure.vcf
+mv ieu-b-5067.vcf outcome.vcf
 
 echo "Process completed!"
